91241094d43ec8e5f8abaaed8617f1a5d5473215
Guillaume-Csr <guillaume.cuisinier33@gmail.com>
2022-04-22 12:20:17 +0200

    pass test to pr

--- tests/test_gameinfo.py
+++ tests/test_gameinfo.py
@@ -10,11 +10,9 @@ from sources.gameinfo import GameInfo as gi
 class GameInfo_Tests(unittest.TestCase):
 
     def test_save_actions(self):
-        outfile = gi.actionsFileName        #   Init file
-        test_content = ["content"]          #   Init content
-        gi.save_actions(outfile, test_content)      #   Save test file
+        outfile = gi.actionsFileName        # Init file
+        test_content = ["content"]          # Init content
+        gi.save_actions(outfile, test_content)      # Save test file
         outfile.seek(0)
-        content = outfile.read()        #   Reading of test file
-        self.assertEqual(content, "content")        #   Compare data with 
-
-
+        content = outfile.read()        # Reading of test file
+        self.assertEqual(content,"content")        # Compare data with 
