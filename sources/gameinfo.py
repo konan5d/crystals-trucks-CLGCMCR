bc2fc3031cad6d8d9db8594fb355bf754e6e3f5d
Guillaume-Csr <guillaume.cuisinier33@gmail.com>
2022-04-22 11:50:16 +0200

    [CmdFile] add save method and unitary test
    - TODO mettre en commun les variables a utiliser
    
    Signed-off-by: Guillaume-Csr <guillaume.cuisinier33@gmail.com>

--- sources/gameinfo.py
+++ sources/gameinfo.py
@@ -1,9 +1,53 @@
 # import sources.game as game
 
 
+from asyncio.windows_events import NULL
+
+import os
+
+
 class GameInfo:
     """Class contenant toutes les informations du jeu"""
+
+    # save_actions data needed ---------------
+    actionsFileName = "Game_Actions.txt"
+    # grid
+    # all game info
+    # actions_list
 
     def __init__(self) -> None:
         """Constructeur de la class"""
         pass
+
+
+    def save_actions(self, actionsFileName, actions):
+        
+        #   Check existance
+        if os.path.exists(actionsFileName):
+
+            os.remove(actionsFileName)  #   Remove if exist
+
+        else:
+
+            # Create file 
+            CmdFile = open(actionsFileName, "w+")
+
+        # TODO insert game info in the output file ---------------------
+        
+        # TODO insert map in the ouput command file --------------------
+        CmdFile.write("###Grid###\n")
+
+        # insert here using writelines(gameGrid)
+
+        CmdFile.write("###End Grid###\n")
+
+        # Write actions part-------------------------------------------
+
+        CmdFile.write("Start !\n")    #   fisrt 
+
+        # Write all actions
+        for i in range(len(actions)):
+            CmdFile.write(actions[i] + "\n")
+
+        # Close file to finish
+        CmdFile.close()
--- tests/test_gameinfo.py
+++ tests/test_gameinfo.py
@@ -1,1 +1,20 @@
 #  import sources.gameinfo as gameinfo
+
+import unittest
+
+from io import StringIO
+
+from sources.gameinfo import GameInfo as gi
+
+
+class GameInfo_Tests(unittest.TestCase):
+
+    def test_save_actions(self):
+        outfile = gi.actionsFileName        #   Init file
+        test_content = ["content"]          #   Init content
+        gi.save_actions(outfile, test_content)      #   Save test file
+        outfile.seek(0)
+        content = outfile.read()        #   Reading of test file
+        self.assertEqual(content, "content")        #   Compare data with 
+
+
