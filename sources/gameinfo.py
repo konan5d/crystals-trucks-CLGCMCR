02dd020cd7e12ec4a917a4dc0dceb9d859d0ca05
Corentin LEGEAY <corentin.legeay@gmail.com>
2022-04-22 09:26:46 +0200

    Create class GameInfo

---
+++ sources/gameinfo.py
@@ -0,0 +1,9 @@
+# import sources.game as game
+
+
+class GameInfo:
+    """Class contenant toutes les informations du jeu"""
+
+    def __init__(self) -> None:
+        """Constructeur de la class"""
+        pass
