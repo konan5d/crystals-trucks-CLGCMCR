4eb1e44e936e0961c076148385d9ff8db0243074
Corentin LEGEAY <corentin.legeay@gmail.com>
2022-04-24 18:28:47 +0200

    GamePlay: add method to get the next pos of truck

--- sources/gameplay.py
+++ sources/gameplay.py
@@ -7,12 +7,15 @@ class GamePlay:
         self.game_info = GameInfo(self.args.game_number, self.args.output_file)
         self.game_info.init_game_info()
 
+    def get_next_trucks_pos(self, truck):
+        pass
+
     def play_trucks(self):
         for truck in self.game_info.get_trucks():
             if truck.last_turn_played != self.game_info.nb_turn:
                 if self.game_info.is_crystal_available(truck.pos_x, truck.pos_y):
-                    print("Truck dig !")
-                    truck.action_dig()
+                    self.game_info.add_actions(truck.action_dig())
+
                 truck.last_turn_played += 1
 
         self.game_info.nb_turn += 1
