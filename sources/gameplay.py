65dc02e4042e65e38a62042cde3d6ed4e41fe51f
Corentin LEGEAY <corentin.legeay@gmail.com>
2022-04-24 17:45:53 +0200

    GamePlay: change import of GameInfo and init GameInfo in GamePlay constructor

--- sources/gameplay.py
+++ sources/gameplay.py
@@ -1,11 +1,11 @@
-from gameinfo.gameinfo import parse_argument, GameInfo
+from gameinfo import parse_argument, GameInfo
 
 
 class GamePlay:
     def __init__(self) -> None:
         self.args = parse_argument()
-
         self.game_info = GameInfo(self.args.game_number, self.args.output_file)
+        self.game_info.init_game_info()
 
 
 if __name__ == "__main__":
