7c68b56438f08573f79c0ce14ff1af4edaab348a
Guillaume-Csr <guillaume.cuisinier33@gmail.com>
2022-04-28 12:57:40 +0200

    fix flake8 error

--- sources/gameplay.py
+++ sources/gameplay.py
@@ -11,23 +11,24 @@ class GamePlay:
     def get_truck_actions_available(self, truck: Truck):
         # Clear actions list
         truck.actions_available.clear()
-        # If crystal is present on actual position 
+        # If crystal is present on actual position
         if self.game_info.is_crystal_available(truck.pos_x, truck.pos_y):
             # Make dig action
             print("is_crystal_available = true")
             truck.actions_available.append(truck.action_dig())
 
         else:
-            # Move in other position 
+            # Move in other position
             truck.actions_available = self.game_info.is_movable(
-            truck, truck.pos_x, truck.pos_y)
+                truck, truck.pos_x, truck.pos_y
+            )
 
     def get_next_trucks_action(self, truck: Truck):
         self.get_truck_actions_available(truck)
         print(truck.actions_available)
 
         if truck.actions_available == truck.action_dig:
-                return truck.action_dig
+            return truck.action_dig
         else:
             return random.choice(truck.actions_available)
 
@@ -36,7 +37,9 @@ class GamePlay:
             if truck.last_turn_played != self.game_info.nb_turn:
                 # self.get_truck_actions_available(truck)
                 tr_action = self.get_next_trucks_action(truck)
-                truck.set_move(tr_action, self.game_info.map_height, self.game_info.map_width)
+                truck.set_move(
+                    tr_action, self.game_info.map_height, self.game_info.map_width
+                )
                 # print("Camion {} play {}".format(truck.id, tr_action))
                 self.game_info.add_actions(tr_action)
 
